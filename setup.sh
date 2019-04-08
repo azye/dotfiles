4b4954de4b2277e9e31cb158ab010da5c01b27b1
Alex Ye <alexzye1@gmail.com>
2019-04-08 00:50:28 -0700

    Add fixes to the setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CONFIG_PATH=`dirname $BASH_SOURCE`
-echo $CONFIG_PATH
+
+CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc.bak
@@ -8,10 +8,6 @@ fi
 
 if [ -f ~/.bash_aliases ]; then
 	mv ~/.bash_aliases ~/.bash_aliases.bak
-fi
-
-if [ -f ~/.bash_env ]; then
-	mv ~/.bash_env ~/.bash_env.bak
 fi
 
 ln -s $CONFIG_PATH/.bashrc ~/.bashrc

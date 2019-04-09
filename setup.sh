2ee9f2e7d79b9158ad8533a4a9649cbfbbb4296e
Alex Ye <alexzye1@gmail.com>
2019-04-08 23:32:09 -0700

    Remove file check that didnt work for now

--- setup.sh
+++ setup.sh
@@ -2,13 +2,8 @@
 
 CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ -f ~/.bashrc ]; then
-	mv ~/.bashrc ~/.bashrc.bak
-fi
-
-if [ -f ~/.bash_aliases ]; then
-	mv ~/.bash_aliases ~/.bash_aliases.bak
-fi
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.bash_aliases ~/.bash_aliases.bak
 
 ln -s $CONFIG_PATH/.bashrc ~/.bashrc
 ln -s $CONFIG_PATH/.bash_aliases ~/.bash_aliases

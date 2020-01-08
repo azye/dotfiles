9ae5036a1a50489f0954af5e3babe83e8d9e8380
Alex Ye <alexzye1@gmail.com>
2020-01-07 16:49:37 -0800

    Update 01_07_20

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 
 CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -s "${CONFIG_PATH}"/.bashrc ~/.bashrc
-ln -s "${CONFIG_PATH}"/.bash_aliases ~/.bash_aliases
-ln -s "${CONFIG_PATH}"/.vimrc ~/.vimrc
-ln -s "${CONFIG_PATH}"/.git-completion.bash ~/.git-completion.bash
-ln -s "${CONFIG_PATH}"/.iterm2.bash ~/.iterm2.bash
+DOT_FILES=(".bashrc" ".bash_aliases" ".vimrc" ".git_completion")
 
-. ~/.bashrc
+for file in "${DOT_FILES[@]}"; 
+do
+	if test -f ~/"${file}"; then
+		unlink ~/"${file}"
+	fi
+
+	ln -s "${CONFIG_PATH}"/"${file}" ~/"${file}"
+done

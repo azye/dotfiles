8fce62e8f4b066fd3fe099ba48a831cebd2fcd4a
Alex Ye <alexzye1@gmail.com>
2021-10-10 17:33:18 -0700

    Just remove the symlink

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
+set -x
 CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DOT_FILES=(".bashrc" ".bash_aliases" ".vimrc" ".git_completion" ".tmux.conf")
 
 for file in "${DOT_FILES[@]}"; 
 do
-	if test -f ~/"${file}"; then
-		rm ~/"${file}"
-	fi
+	rm ~/"${file}"
 
 	ln -s "${CONFIG_PATH}"/"${file}" ~/"${file}"
 done
